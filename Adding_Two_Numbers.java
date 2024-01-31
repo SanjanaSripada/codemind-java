3f9a991386bcd2e30fcd25798634f610961fa3a7
SanjanaSripada <119917997+SanjanaSripada@users.noreply.github.com>
2024-01-31 09:59:59 +0530

    import java.io.*; import java.util.Scanner; public class Adding {     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         int a = sc.nextInt();         int b = sc.nextInt();         int tot;         tot = a + b;         System.out.println(tot);     } }

---
+++ Adding_Two_Numbers.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.Scanner;
+public class Adding
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int tot;
+        tot = a + b;
+        System.out.println(tot);
+    }
+}
