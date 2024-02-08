bbd766816998b86293fe901143cd32bd89976790
SanjanaSripada <119917997+SanjanaSripada@users.noreply.github.com>
2024-02-08 21:07:22 +0530

    import java.io.*; import java.util.*; public class Value {     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         char input = sc.next().charAt(0);         int ascii = input;         System.out.println(ascii);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class Value
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        char input = sc.next().charAt(0);
+        int ascii = input;
+        System.out.println(ascii);
+    }
+}
