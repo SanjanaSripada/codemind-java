a39443c184175e472d0a343e89e1c01f43bb68d4
SanjanaSripada <119917997+SanjanaSripada@users.noreply.github.com>
2024-02-12 10:22:35 +0530

    import java.io.*; import java.util.*; public class Dragon {     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         int X = sc.nextInt();         if(X % 2 == 0 && X != 2)         {             System.out.println("YES");         }         else         {             System.out.println("NO");         }     } }

---
+++ Dragon_Fruit.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+public class Dragon
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+        if(X % 2 == 0 && X != 2)
+        {
+            System.out.println("YES");
+        }
+        else
+        {
+            System.out.println("NO");
+        }
+    }
+}
