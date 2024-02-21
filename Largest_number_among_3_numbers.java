2d5536983530b84b534e69806b3c6555b10d4e1a
SanjanaSripada <119917997+SanjanaSripada@users.noreply.github.com>
2024-02-21 10:13:43 +0530

    import java.io.*; import java.util.*; public class Largest {     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         int A = sc.nextInt();         int B = sc.nextInt();         int C = sc.nextInt();         if(A > B)         {             System.out.println(A);         }         else if(B > C)         {             System.out.println(B);         }         else         {             System.out.println(C);         }     } }

---
+++ Largest_number_among_3_numbers.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+public class Largest
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        if(A > B)
+        {
+            System.out.println(A);
+        }
+        else if(B > C)
+        {
+            System.out.println(B);
+        }
+        else
+        {
+            System.out.println(C);
+        }
+    }
+}
